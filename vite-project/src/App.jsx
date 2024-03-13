f8c1c159aff60950c8acee1500833bab9806192e
kumar3mihir <mihirkumarmallick@Mihirs-MacBook-Air.local>
2024-03-14 01:52:02 +0530

    Calender using react and tailwind css
    13th mar 2024

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import MessMenu from './pages/MessMenu';
+
+function App() {
+  return (
+    <>
+      <MessMenu />
+    </>
+  );
+}
+
+export default App
